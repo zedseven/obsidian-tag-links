e4e6dbdd82729beaae0526a4cebfc5dee65308ac
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2024-03-03 13:25:21 -0500

    Implement the basic functionality.

--- main.ts
+++ main.ts
@@ -1,86 +1,48 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+    App,
+    Editor,
+    Notice,
+    Plugin,
+    PluginSettingTab,
+    Setting
+} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface TagLinkSettings {
+	tagLinkSubstitutions: Array<TagLinkSubstitution>;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface TagLinkSubstitution {
+    tagRegex: string;
+    linkSubstitution: string;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: TagLinkSettings = {
+    tagLinkSubstitutions: new Array<TagLinkSubstitution>(),
+}
+
+const DEFAULT_SUBSTITUTION: TagLinkSubstitution = {
+    tagRegex: '',
+    linkSubstitution: '',
+}
+
+export default class TagLink extends Plugin {
+	settings: TagLinkSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			id: 'open-tag-as-link',
+			name: 'Open tag as a link',
+			editorCallback: async (editor, _) => {
+                await this.openTagAsLinkAtCursor(this, editor);
+            }
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new TagLinkSettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -89,28 +51,68 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+    async openTagAsLinkAtCursor(plugin: TagLink, editor: Editor) {
+        // https://help.obsidian.md/Editing+and+formatting/Tags#Tag+format
+        const findTagRegex = /#([\w\/\-_]+)/g;
+
+        const cursorPosition = editor.getCursor();
+        const currentLine = editor.getLine(cursorPosition.line);
+
+        let searchResult;
+        while ((searchResult = findTagRegex.exec(currentLine)) !== null) {
+            if (findTagRegex.lastIndex < cursorPosition.ch) {
+                continue;
+            }
+            if (searchResult.index > cursorPosition.ch) {
+                break;
+            }
+
+            const selectedTag = searchResult[1];
+            await plugin.openTagAsLink(selectedTag);
+
+            break;
+        }
+    }
+
+    async openTagAsLink(tag: string) {
+        for (const tagLinkSubstitution of this.settings.tagLinkSubstitutions) {
+            const MINIMUM_GROUP_LENGTH = 2;
+
+            let tagRegex;
+            try {
+                tagRegex = new RegExp(tagLinkSubstitution.tagRegex);
+            } catch (_) {
+                continue;
+            }
+
+            let matchResult = tag.match(tagRegex);
+            if (matchResult === null) {
+                continue;
+            }
+
+            if (matchResult.length < MINIMUM_GROUP_LENGTH) {
+                continue;
+            }
+
+            let finalLink = tagLinkSubstitution.linkSubstitution;
+            for (let index = 1; index < matchResult.length; index++) {
+                finalLink = finalLink.replace('$' + index, matchResult[index]);
+            }
+
+            window.open(finalLink);
+
+            return;
+        }
+
+        new Notice('The selected tag doesn\'t match any configured tag link substitutions!');
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class TagLinkSettingTab extends PluginSettingTab {
+	plugin: TagLink;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TagLink) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,15 +122,51 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+        containerEl.createEl('h2', { text: 'Tag Link Substitutions' });
+
+        new Setting(containerEl)
+            .setName('Add New')
+            .addButton(button => button
+                .setIcon('plus')
+                .setTooltip('Add')
+                .onClick(async () => {
+                    this.plugin.settings.tagLinkSubstitutions.push({ ...DEFAULT_SUBSTITUTION });
+
+                    await this.plugin.saveSettings();
+                    this.display();
+                }));
+
+        for (let index = 0; index < this.plugin.settings.tagLinkSubstitutions.length; index++) {
+            const tagLinkSubstitution = this.plugin.settings.tagLinkSubstitutions[index];
+
+            const row = new Setting(containerEl)
+                .addText(text => text
+                    .setPlaceholder('Tag Regex')
+                    .setValue(tagLinkSubstitution.tagRegex)
+                    .onChange(async (newValue) => {
+                        this.plugin.settings.tagLinkSubstitutions[index].tagRegex = newValue;
+
+                        await this.plugin.saveSettings();
+                    }));
+
+            row.addText(text => text
+                .setPlaceholder('Link Substitution')
+                .setValue(tagLinkSubstitution.linkSubstitution)
+                .onChange(async (newValue) => {
+                    this.plugin.settings.tagLinkSubstitutions[index].linkSubstitution = newValue;
+
+                    await this.plugin.saveSettings();
+                }));
+
+            row.addButton(button => button
+                .setIcon('x')
+                .setTooltip('Delete')
+                .onClick(async () => {
+                    this.plugin.settings.tagLinkSubstitutions.splice(index, 1);
+
+                    await this.plugin.saveSettings();
+                    this.display();
+                }));
+        }
 	}
 }
